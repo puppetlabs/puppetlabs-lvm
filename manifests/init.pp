8fbe41bd7cd08796521a82304b3b8b94fa076d8e
Bruce Williams <bruce@codefluency.com>
2010-02-07 21:46:24 -0800

    Support different ordering for different ensure values.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,30 @@
-define puppet-lvm::volume($vg, $pv, $lv, $fstype, $size = undef) {
-  physical_volume { $pv: ensure => present }
-  volume_group { $vg: ensure => present, physical_volumes => $pv }
-  logical_volume { $lv: ensure => present, volume_group => $vg, size => $size }
-  filesystem { "/dev/${vg}/${lv}": ensure => $fstype }
+define lvm::volume($vg, $pv, $fstype, $size = undef, $ensure) {
+  case $ensure {
+    #
+    # Clean up the whole chain.
+    #
+    cleaned: {
+      physical_volume { $pv: ensure => present }
+      volume_group { $vg: ensure => present, physical_volumes => $pv, before => Physical_volume[$pv] }
+      logical_volume { $name: ensure => present, volume_group => $vg, size => $size, before => Volume_group[$vg] }
+    }
+    #
+    # Just clean up the logical volume
+    #
+    absent: {
+      logical_volume { $name: ensure => absent, volume_group => $vg, size => $size }
+    }
+    #
+    # Create the whole chain.
+    #
+    present: {
+      physical_volume { $pv: ensure => present }
+      volume_group { $vg: ensure => present, physical_volumes => $pv, require => Physical_volume[$pv] }
+      logical_volume { $name: ensure => present, volume_group => $vg, size => $size, require => Volume_group[$vg] }
+      filesystem { "/dev/${vg}/${name}": ensure => $fstype, require => Logical_volume[$name] }
+    }
+    default: {
+     fail ( 'puppet-lvm::volume: ensure parameter can only be set to cleaned, absent or present' )
+    }
+  }
 }

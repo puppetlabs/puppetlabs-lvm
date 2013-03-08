d19dde9cedb52b184b7600c9b62fb9a2330209dc
Russell Harrison <rharrison@fedoraproject.org>
2013-03-08 14:22:42 -0500

    Add RDoc / License header to the volume define

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -1,4 +1,64 @@
-define lvm::volume($vg, $pv, $fstype = undef, $size = undef, $ensure) {
+# == Define: lvm::volume
+#
+# This defined type will create a <code>logical_volume</code> with the name of the define and ensure a <code>physical_volume</code>,
+# <code>volume_group</code>, and <code>filesystem</code> resource have been created on the block device supplied.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Can only be set to <code>cleaned</code>, <code>absent</code> or <code>present</code>. A value of <code>present</code> will
+#   ensure that the <code>physical_volume</code>, <code>volume_group</code>, <code>logical_volume</code>, and
+#   <code>filesystem</code> resources are present for the volume. A value of <code>cleaned</code> will ensure that all of the
+#   resources are <code>absent</code> <b>Warning this has a high potential for unexpected harm</b> use it with caution. A value of
+#   <code>absent</code> will remove only the <code>logical_volume</code> resource from the system.
+# [*pv*]
+#   The block device to ensure a <code>physical_volume</code> has been created on.
+# [*vg*]
+#   The <code>volume_group</code> to ensure is created on the <code>physical_volume</code> provided by the <code>pv</code>
+#   parameter.
+# [*fstype*]
+#   The type of <code>filesystem</code> to create on the logical volume.
+# [*size*]
+#   The size the <code>logical_voluem</code> should be.
+#
+# === Examples
+#
+# Provide some examples on how to use this type:
+#
+#   lvm::volume { 'lv_example0':
+#     vg     => 'vg_example0',
+#     pv     => '/dev/sdd1',
+#     fstype => 'ext4',
+#     size => '100GB',
+#   }
+#
+# === Copyright
+#
+# See README.markdown for the module author information.
+#
+# === License
+#
+# This file is part of the puppetlabs/lvm puppet module.
+#
+# puppetlabs/lvm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, version 2 of the License.
+#
+# puppetlabs/lvm is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with puppetlabs/lvm. If not, see http://www.gnu.org/licenses/.
+#
+define lvm::volume (
+  $ensure,
+  $pv,
+  $vg,
+  $fstype = undef,
+  $size   = undef
+) {
   case $ensure {
     #
     # Clean up the whole chain.

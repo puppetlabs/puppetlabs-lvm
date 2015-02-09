e315c2de4f24fcf5c944da4fcc81a014f25f409c
Rhommel Lamas <roml@rhommell.com>
2015-02-09 16:03:24 +0100

    Add lvm2 package installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,16 @@
 # == Class: lvm
 #
 class lvm (
-  $volume_groups = {},
+  $package_ensure = 'installed',
+  $manage_pkg     = undef,
+  $volume_groups  = {},
 ) {
+
+  if $::manage_pkg {
+    package { 'lvm2':
+      ensure   => $package_ensure
+    }
+  }
 
   validate_hash($volume_groups)
 

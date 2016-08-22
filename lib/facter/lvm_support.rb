41a712913657e66bed41d58972c65f6d38b54d3f
Elyse Salberg <elyse_salberg@putnam.com>
2016-08-22 13:46:44 -0400

    Change Facter::Util::Resolution to Facter::Core::Execution and add timeout: 30 to options hash; add if around vgs and pvs commands to handle F::C::S new management of nil commands; attempt to update spec

--- lib/facter/lvm_support.rb
+++ lib/facter/lvm_support.rb
@@ -14,7 +14,11 @@ end
 vg_list = []
 Facter.add('lvm_vgs') do
   confine :lvm_support => true
-  vgs = Facter::Util::Resolution.exec('vgs -o name --noheadings 2>/dev/null')
+
+  lvm = Facter.value(:lvm_support)
+  if lvm 
+    vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', timeout: 30)
+  end
   if vgs.nil?
     setcode { 0 }
   else
@@ -29,7 +33,7 @@ vg_list.each_with_index do |vg, i|
   Facter.add("lvm_vg_#{i}") { setcode { vg } }
   Facter.add("lvm_vg_#{vg}_pvs") do
     setcode do
-      pvs = Facter::Util::Resolution.exec("vgs -o pv_name #{vg} 2>/dev/null")
+      pvs = Facter::Core::Execution.execute("vgs -o pv_name #{vg} 2>/dev/null", timeout: 30)
       res = nil
       unless pvs.nil?
         res = pvs.split("\n").select{|l| l =~ /^\s+\// }.collect(&:strip).sort.join(',')
@@ -44,7 +48,11 @@ end
 pv_list = []
 Facter.add('lvm_pvs') do
   confine :lvm_support => true
-  pvs = Facter::Util::Resolution.exec('pvs -o name --noheadings 2>/dev/null')
+
+  lvm = Facter.value(:lvm_support)
+  if lvm 
+    pvs = Facter::Core::Execution.execute('pvs -o name --noheadings 2>/dev/null', timeout: 30)
+  end
   if pvs.nil?
     setcode { 0 }
   else

cdf4887aba3a5f736a828ead16e27254b98f9975
Markus Weippert <markus@gekmihesg.de>
2021-04-21 14:14:56 +0200

    Be consistent when calculating sizes
    Ensure that passing the size as integer yields the same result as
    passing the size as float.
    Also support sizes below 1 KiB.

--- functions/bytes_to_size.pp
+++ functions/bytes_to_size.pp
@@ -2,6 +2,7 @@ function lvm::bytes_to_size (
   Numeric $size,
 ) {
   $units = {
+    'b' => 1,
     'k' => 1024,
     'm' => 1048576,
     'g' => 1073741824,
@@ -18,7 +19,7 @@ function lvm::bytes_to_size (
   # Use the last unit
   $largest_unit = $remaining_units.keys[-1]
 
-  $value = ($size / $units[$largest_unit])
+  $value = (Float($size) / $units[$largest_unit])
 
   # # Return the string
   "${value}${largest_unit}"

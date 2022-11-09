c399ec2fac9efbfcf3e52605747bc3bec2650212
martyewings <martyewings@gmail.com>
2022-11-09 11:20:27 +0000

    add lvm2 package to test platforms

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -3,8 +3,19 @@ $LOAD_PATH.unshift(File.join(dir, 'lib'))
 
 require 'helpers'
 require 'matchers'
+require 'singleton'
+require 'serverspec'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
   config.include Helpers
   config.include Matchers
+  c.before :suite do
+    pp = <<-MANIFEST
+      package { 'lvm2':
+        ensure => 'latest',
+      }
+    MANIFEST
+    LitmusHelper.instance.apply_manifest(pp)
+  end
 end

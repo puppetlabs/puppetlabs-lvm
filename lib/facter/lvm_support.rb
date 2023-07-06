7d8052b168211399a9e4db5efb779a2c3cbb2117
Tibor Répási <rtib@users.noreply.github.com>
2023-07-06 11:39:29 +0200

    fix tests

--- lib/facter/lvm_support.rb
+++ lib/facter/lvm_support.rb
@@ -17,9 +17,7 @@ vg_list = []
 Facter.add('lvm_vgs') do
   confine lvm_support: true
 
-  if Facter.value(:lvm_support)
-    vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', timeout: 30)
-  end
+  vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', timeout: 30)
 
   if vgs.nil?
     setcode { 0 }
@@ -50,10 +48,8 @@ end
 pv_list = []
 Facter.add('lvm_pvs') do
   confine lvm_support: true
-  if Facter.value(:lvm_support)
-    pvs = Facter::Core::Execution.execute('pvs -o name --noheadings 2>/dev/null', timeout: 30)
-  end
 
+  pvs = Facter::Core::Execution.execute('pvs -o name --noheadings 2>/dev/null', timeout: 30)
   if pvs.nil?
     setcode { 0 }
   else

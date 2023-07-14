4c5ab5a6ecb8a1c09720c1a4136c3399437ff3fe
Tibor Répási <rtib@users.noreply.github.com>
2023-07-14 16:51:03 +0200

    add volume_group_map fact

---
+++ lib/facter/volume_group_map.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Facter.add(:volume_group_map) do
+  # Fact should be confined to only linux servers that have the lvs command
+  confine do
+    Facter.value('kernel') == 'Linux' &&
+      Facter::Core::Execution.which('vgs')
+  end
+
+  setcode do
+    Facter.value(:volume_groups).keys.to_h do |vg|
+      [
+        vg,
+        Facter::Core::Execution.exec("vgs -o pv_name #{vg} --noheading --nosuffix")
+          .split("\n")
+          .map { |x| x.strip }
+          .join(',')
+      ]
+    end
+  end
+end
